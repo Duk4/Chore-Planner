31ad3a661001592211f8db957ba304566e631c80
Dule <crashendodt@gmail.com>
2020-05-25 09:33:23 +0200

    update

--- components/ListItemButton.js
+++ components/ListItemButton.js
@@ -3,11 +3,21 @@ import { TouchableOpacity } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const ListItemButton = ({ name, style, navigationHandler }) => {
-    return (
-        <TouchableOpacity onPress={() => { navigationHandler() }} style={style}>
+    let buttonRender = (
+        <TouchableOpacity onPress={() => { console.log(name) }} style={style}>
             <MaterialCommunityIcons name={name} size={20} color="#2c2d2e" />
         </TouchableOpacity>
     );
+
+    if (navigationHandler) {
+        buttonRender = (
+            <TouchableOpacity onPress={() => { navigationHandler() }} style={style}>
+                <MaterialCommunityIcons name={name} size={20} color="#2c2d2e" />
+            </TouchableOpacity>
+        );
+    };
+
+    return buttonRender;
 };
 
 export default ListItemButton;
